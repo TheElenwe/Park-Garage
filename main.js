df27bdae8d177f10d5556cd194e41bd0fddbe261
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-27 00:23:14 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 let readlineSync = require("readline-sync");
 
-let hoursParked = readlineSync.question("Please input how many hours you have spent \n");
+
 let memberStatus = readlineSync.question("Input yes if you're a member, no if you're not. \n");
 
+memberStatus=parseFloat(memberStatus=2);
 
- hoursParked=parseFloat(hoursParked) 
-
-if (memberStatus === "yes") {
-  console.log(hoursParked +1.5)
+if(memberStatus === "yes") {
+  console.log(hoursParked )
 }else if (memberStatus ==="no"){
-console.log(hoursParked +3)
+console.log(hoursParked)
 }
+
+let hoursParked = readlineSync.question("how hours parked \n");
+hoursParked=parseFloat(hoursParked=2);
+  if (hoursParked===2){
+  hoursParked=2;
+ 
+}
+else if(hoursParked==3){
+  hoursParked=3;
+  
+}
+total=hoursParked+hoursParked+memberStatus;
+console.log(total)
+
