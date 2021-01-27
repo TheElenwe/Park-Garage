f34f3ffb92ffb4cbf816e6ed3112ab3e9a7ca088
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-27 18:06:20 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,30 +1,27 @@
 let readlineSync = require("readline-sync");
-
 let memberStatus = readlineSync.question("Input yes if you're a member, no if you're not. \n");
+let hoursParked = Number(readlineSync.question("how hours parked \n"));
 if (memberStatus === "yes") {
   memberStatus = parseFloat((memberStatus = 1.5));
 } else if (memberStatus === "no") {
   memberStatus = parseFloat((memberStatus = 3));
 }
 while(true){
-  let hoursParked1 = Number(readlineSync.question("how hours parked \n"));
-let hoursParked2 = Number(readlineSync.question("how hours parked \n"));
-let hoursParked3 = Number(readlineSync.question("how hours parked \n"));
-let hoursParked4 = Number(readlineSync.question("how hours parked \n"));
-if (hoursParked1 === "ilias") {
-  hoursParked1 = 2;
+if (hoursParked === 1) {
+  hoursParked = 2;
+  total=hoursParked+memberStatus
+  console.log(total)
   break;
-} else if (hoursParked2 === 2) {
-  hoursParked2 = 1.5;
 
+} else if (hoursParked === 2) {
+  hoursParked = 1.5;
+  totalAmount=hoursParked+hoursParked+memberStatus;
+console.log(totalAmount)
+  
  break;
-} else if (hoursParked3 === 3) {
-  hoursParked3 = 1;
-  break;
+
 } 
-else if (hoursParked4 === 4) {
-  housParked4 = 0.5;
-  break;
+
 }
-}
+
 
