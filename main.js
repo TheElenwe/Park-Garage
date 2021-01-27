ba0387ab04e3cbf2e8f36c4dcc88f4538805f091
TheElenwe <73945429+TheElenwe@users.noreply.github.com>
2021-01-27 16:53:59 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,30 @@
 let readlineSync = require("readline-sync");
 
 let memberStatus = readlineSync.question("Input yes if you're a member, no if you're not. \n");
-let hoursParked = Number(readlineSync.question("how hours parked \n"));
-
 if (memberStatus === "yes") {
-  memberStatus=parseFloat(memberStatus=1.5)
+  memberStatus = parseFloat((memberStatus = 1.5));
 } else if (memberStatus === "no") {
-  memberStatus=parseFloat(memberStatus=3)
+  memberStatus = parseFloat((memberStatus = 3));
+}
+while(true){
+  let hoursParked1 = Number(readlineSync.question("how hours parked \n"));
+let hoursParked2 = Number(readlineSync.question("how hours parked \n"));
+let hoursParked3 = Number(readlineSync.question("how hours parked \n"));
+let hoursParked4 = Number(readlineSync.question("how hours parked \n"));
+if (hoursParked1 === "ilias") {
+  hoursParked1 = 2;
+  break;
+} else if (hoursParked2 === 2) {
+  hoursParked2 = 1.5;
+
+ break;
+} else if (hoursParked3 === 3) {
+  hoursParked3 = 1;
+  break;
+} 
+else if (hoursParked4 === 4) {
+  housParked4 = 0.5;
+  break;
+}
 }
 
-if (hoursParked === 2) {
-  hoursParked = 2;
-} else if (hoursParked == 3) {
-  hoursParked = 3;
-}
-total = hoursParked +  memberStatus;
-console.log(total);
